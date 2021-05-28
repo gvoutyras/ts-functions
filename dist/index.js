cc4498d66ebc5af851e74a042af739ee0eaed588
George Voutyras <geovoutyras@gmail.com>
2021-05-28 23:30:27 +0300

    implemeted w/ classes

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSymbol = void 0;
+exports.getSymbol = require("./getSymbol");
 //# sourceMappingURL=index.js.map
