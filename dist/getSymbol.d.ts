89da3a06df6e904ef5a6bfe1eee5ee924a062ad0
George Voutyras <geovoutyras@gmail.com>
2021-05-29 01:14:17 +0300

    temp

--- dist/getSymbol.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class getSymbol {
-    getSymbol(curCode: String): String | "$" | "лв" | "R$" | "&yen;" | "&euro;" | "₨" | "Rp" | "&pound;" | "руб" | "kr" | "TL" | "zł";
-}
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSymbol = void 0;
-exports.getSymbol = require("./getSymbol");
-//# sourceMappingURL=index.js.map
